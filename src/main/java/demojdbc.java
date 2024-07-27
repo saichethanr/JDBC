87868eefd19ec6d1305ab1fb1e3a71b1702d6fba
saichethanr <saichethan9019@gmail.com>
2024-07-27 23:11:45 +0530

    updated

--- src/main/java/demojdbc.java
+++ src/main/java/demojdbc.java
@@ -14,10 +14,29 @@ public  class demojdbc {
       String url = "jdbc:postgresql://127.0.0.1:5432/practice";
       String user = "sai";
       String password = "1445";
+      String query = "select sname from student where sid = 1";
       //load and register the Driver
       Class.forName("org.postgresql.Driver");
       //create a connection
       Connection con = DriverManager.getConnection(url,user,password);
+      Statement st = con.createStatement();
+      //the execute query retunra a result set as the answer
+      ResultSet result = st.executeQuery(query);
+
+      //to check is any data is received or not we need to check if the result is valid or not so
+      //this just print true or false if the data exists or not
+      System.out.println(  result.next());
+
+      //if we have to print the data
+      //then we have to do one thing
+      //before we fetch the data the pointer will be on the prev row of the first row hence to fetch the first row
+      //using getString we nwed to use  rs.next()
+      result.next();
+      String name = result.getString("sname");
+      System.out.println(name);
+      con.close();
+      System.out.println("connection close");
+
       System.out.println("connection established");
   }
 }
