07ffe23babf63dd7a31efcf659b1106d07b4d453
saichethanr <saichethan9019@gmail.com>
2024-07-28 08:08:48 +0530

    CRUD operations done

---
+++ src/main/java/Insert.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Insert {
+    public static void main(String [] args) throws  Exception{
+           /*
+      import the pakage
+      load and register
+      create connection
+      create a statment
+      execute the statement
+      process results
+      close
+      */
+
+        String url = "jdbc:postgresql://127.0.0.1:5432/practice";
+        String user = "sai";
+        String password = "1445";
+        String query1 = "INSERT INTO student values  (7,'Kiran',48)";
+        String query2 = "UPDATE  student SET sname='Vikyath where sid =1'";
+        String query3 = "DELETE FROM student where sid=5";
+        //load and register the Driver
+        Class.forName("org.postgresql.Driver");
+        //create a connection
+        Connection con = DriverManager.getConnection(url,user,password);
+        System.out.println("connection established");
+        Statement st = con.createStatement();
+        //executequery is used when we want to get data
+        //the execute is used to insert the date into the table
+        //it will return false if executed any insert update or delete query
+        boolean state =  st.execute( query3);
+        System.out.println(state);
+
+
+        con.close();
+        System.out.println("connection close");
+    }
+}
--- src/main/java/Read.java
+++ src/main/java/Read.java
@@ -1,5 +1,5 @@
 import java.sql.*;
-public  class demojdbc {
+public  class Read {
   public static void main(String [] args) throws Exception {
       /*
       import the pakage
